b35db24ff0c5ba3d99786d465e2683a15b182025
angelica <angelica.cavero@gmail.com>
2022-11-01 23:42:30 +0100

    experience added

---
+++ src/components/Experience.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Experience.css";
+const Experience = ({ experience }) => {
+  return (
+    <div className="card">
+      {experience.map((item) => {
+        return (
+          <div key={JSON.stringify(item)}>
+            <p>📕 {item.name}</p>
+            <p>{item.date}</p>
+            <p>{item.where}</p>
+            <p>{item.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Experience;
